3734f21f8c388a2a504c6e24f65e9cb40bec9127
Ilya <iambren@yandex.ru>
2020-09-26 13:07:44 +0400

    Наш первый коммит

---
+++ JS/les1/script.js
@@ -0,0 +1,17 @@
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14; 
+
+console.log(4/0);
+
+let person = {
+    name: "Ilya",
+    Age: 30,
+    isMarried: true
+};
+
+console.log(person.name);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[1]);
